16511628893b5524d3e61e6133d84bae8e62f246
derylprojects <87072063+derylprojects@users.noreply.github.com>
2021-09-22 23:34:45 -0700

    Create Translate.py

---
+++ Translate.py
@@ -0,0 +1,18 @@
+from pygoogletranslation import Translator
+
+translator = Translator()
+
+def translate(text: str, source: str=EN, destination: str=FA):
+    try:
+        translated_text = translator.translate(text, src=source, dest=destination)
+        return translated_text
+    except:
+        return False
+
+def inline_translate(text: str, source: str=EN, destination: str=FA) -> str:
+    if text == "":
+        return text
+    while True:
+        translated = translate(text, source, destination)
+        if translated != False:
+            return translated.text
